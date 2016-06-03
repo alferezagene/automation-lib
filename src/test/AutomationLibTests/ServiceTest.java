0ecedb8629dd8c315f641b44ad0e39f7efae9aec
sphanges <b@thebteam.co.nz>
2016-06-03 13:48:46 +1200

    Added pom

--- src/test/AutomationLibTests/ServiceTest.java
+++ src/test/AutomationLibTests/ServiceTest.java
@@ -10,7 +10,7 @@ public class ServiceTest {
     @Test
     public void ServiceTest(){
 
-        String loginJSON = "{\"username\":\"wibble@testemail.test\",\"password\": \"111111\"}";
+        String loginJSON = "{\"username\":\"wibble@testemail.test\",\"password\": \"password\"}";
         String loginURL = "https://aws-tnz-api-v2.xstream.dk/family-accounts/login";
         RESTRequest loginPost = new RESTRequest(loginURL,loginJSON, new String[0], "POST");
         String output = loginPost.sendRequest();
