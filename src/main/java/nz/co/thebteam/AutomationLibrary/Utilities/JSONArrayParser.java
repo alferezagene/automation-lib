2c1d7c34003dcf9c80962f0e969ee177d9700a57
Bronwyn Dent <b@Websites-MacBook-Pro.local>
2016-06-24 10:04:07 +1200

    New way to find values under multiple nodes for JSON parser. New jar.

--- src/main/java/nz/co/thebteam/AutomationLibrary/Utilities/JSONArrayParser.java
+++ src/main/java/nz/co/thebteam/AutomationLibrary/Utilities/JSONArrayParser.java
@@ -4,7 +4,9 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class JSONArrayParser {
 
@@ -33,6 +35,7 @@ public class JSONArrayParser {
             return 0;
         }
     }
+
     public String findValueAnyLevel(String nodeName) {
 
         for (int i = 0; i < jsonArray.size(); i++) {
@@ -47,4 +50,37 @@ public class JSONArrayParser {
         }
         return null;
     }
+
+    public List<JSONObject> findNodes(String nodeName) {
+
+        List<JSONObject> nodes = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            //if it's an object
+            if (jsonArray.get(i).getClass().toString().contains("JSONObject")) {
+                JSONObject obj = (JSONObject) jsonArray.get(i);
+                Object[] valuesCollection = obj.values().toArray();
+                Object[] keysCollection = obj.keySet().toArray();
+                //for each element (hashmap) of the object
+                for (int q = 0; q < keysCollection.length; q++) {
+                    //if it's the right node, add it to the list
+                    if (keysCollection[q].equals(nodeName)) {
+                       //sometimes the nodes have an object, sometimes an array with single object
+                        if (valuesCollection[q].getClass().toString().contains("JSONArray")) {
+                         JSONArray valueArray =  (JSONArray) valuesCollection[q];
+                            nodes.add((JSONObject) valueArray.get(0));
+                        } else {
+                            nodes.add((JSONObject) valuesCollection[q]);
+                        }
+                    } else if (valuesCollection[q].getClass().toString().contains("JSONArray")) {
+                        JSONArrayParser j2 = new JSONArrayParser((JSONArray) valuesCollection[q]);
+                       //recursively go down the jsonarrays and return nodes
+                        nodes.addAll(j2.findNodes(nodeName));
+                    }
+                }
+            }
+        }
+        return nodes;
+    }
+
 }
