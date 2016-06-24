12909c638b2161f5d99cb78cba4810d03ecde4a3
Bronwyn Dent <b@Websites-MacBook-Pro.local>
2016-06-24 13:53:55 +1200

    Added a TextPrettifier for pretty printing JSON and for formatting XML for embedding into scenarios so the structure is maintained. New jar.

--- src/main/java/nz/co/thebteam/AutomationLibrary/Utilities/TextPrettifier.java
+++ src/main/java/nz/co/thebteam/AutomationLibrary/Utilities/TextPrettifier.java
@@ -1,12 +1,56 @@
 package nz.co.thebteam.AutomationLibrary.Utilities;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+import java.io.StringWriter;
+
 public class TextPrettifier {
 
 
-    public TextPrettifier(String textToConvert) {
+    public static String convertXMLtoHTMLFriendly(String xmlToConvert) {
 
+        xmlToConvert = prettyFormat(xmlToConvert);
+        xmlToConvert = xmlToConvert.replace("<", "&lt;");
+        xmlToConvert = xmlToConvert.replace(">", "&gt;");
+        return xmlToConvert;
     }
 
+    public static String prettyFormat(String input) {
+        try {
+            Source xmlInput = new StreamSource(new StringReader(input));
+            StringWriter stringWriter = new StringWriter();
+            StreamResult xmlOutput = new StreamResult(stringWriter);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(xmlInput, xmlOutput);
+            return xmlOutput.getWriter().toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e); // simple exception handling, please review it
+        }
+    }
 
+    public static String indentJson(String input) {
+        if (input != null) {
+            String json;
+            JsonParser parser = new JsonParser();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonElement el = parser.parse(input);
+            json = gson.toJson(el);
+            return json;
+        }
+        return null;
 
+    }
 }
